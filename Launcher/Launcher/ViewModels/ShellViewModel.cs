32c5e90ddcc7c6f2399cb7b95197c9c372e5c332
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-08 22:30:49 +0200

    Moved main window to mvvm folders

---
+++ Launcher/Launcher/ViewModels/ShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Launcher.Contracts;
+
+namespace Launcher.ViewModels
+{
+    public sealed class ShellViewModel : Conductor<ITab>, IShell
+    {
+        public ShellViewModel()
+        {
+            DisplayName = "Minecraft Launcher";
+        }
+    }
+}
