d822c3f72235d293cc250bfbd0ff138e3be91660
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-11 13:41:11 +0200

    Basic and empty viewmodels

---
+++ Launcher/Launcher/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Launcher.Models;
+using MahApps.Metro.IconPacks;
+
+namespace Launcher.ViewModels
+{
+    public sealed class HomeViewModel : TabBase
+    {
+        public HomeViewModel()
+        {
+            DisplayName = "Home";
+            DisplayIcon = PackIconMaterialKind.HomeOutline;
+            DisplayOrder = 1;
+            IsHomeTab = true;
+        }
+    }
+}
---
+++ Launcher/Launcher/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Launcher.Models;
+using MahApps.Metro.IconPacks;
+
+namespace Launcher.ViewModels
+{
+    public sealed class SettingsViewModel : TabBase
+    {
+        public SettingsViewModel()
+        {
+            DisplayName = "Settings";
+            DisplayIcon = PackIconMaterialKind.Settings;
+            DisplayOrder = 1;
+            IsShortTab = true;
+        }
+    }
+}
