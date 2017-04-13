ea2af521bd9dabcc1210da52d4d7fa01db365d26
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-13 19:31:37 +0200

    Some new Mojang Api models

---
+++ Launcher/Launcher/Models/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher.Models
+{
+    public class Profile
+    {
+        public Profile()
+        {
+        }
+
+        public Profile(string id, string name, bool legacy)
+        {
+            Id = id;
+            Name = name;
+            Legacy = legacy;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool Legacy { get; set; }
+    }
+}
