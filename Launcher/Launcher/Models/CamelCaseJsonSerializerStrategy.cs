9f8905d529c626a175e3c3f61bddb68d92ec7b0b
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-13 19:29:14 +0200

    CamelCase json properties

---
+++ Launcher/Launcher/Models/CamelCaseJsonSerializerStrategy.cs
@@ -0,0 +1,12 @@
+﻿using RestSharp;
+
+namespace Launcher.Models
+{
+    public class CamelCaseJsonSerializerStrategy : PocoJsonSerializerStrategy
+    {
+        protected override string MapClrMemberNameToJsonFieldName(string clrPropertyName)
+        {
+            return char.ToLower(clrPropertyName[0]) + clrPropertyName.Substring(1);
+        }
+    }
+}
