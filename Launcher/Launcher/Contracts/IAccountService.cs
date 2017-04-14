490212369a419a660293a5f9fc979edd40d7369b
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-15 00:08:38 +0200

    Async + log off

--- Launcher/Launcher/Contracts/IAccountService.cs
+++ Launcher/Launcher/Contracts/IAccountService.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Launcher.Models;
 
 namespace Launcher.Contracts
 {
     public interface IAccountService
     {
-        bool Authenticate(string username, string password);
-        bool Authenticate(string accessToken);
-        UserInfo GetUserInfo();
+        Task<bool> AuthenticateAsync(string username, string password);
+        Task<bool> AuthenticateAsync(string accessToken);
+        Task<bool> LogOff();
+        Task<UserInfo> GetUserInfoAsync();
     }
 }
--- Launcher/Launcher/Services/MojangAccountService.cs
+++ Launcher/Launcher/Services/MojangAccountService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Net;
-using System.Net.Http;
+using System.Threading.Tasks;
 using Launcher.Contracts;
 using Launcher.Models;
 using Launcher.Properties;
@@ -13,9 +12,8 @@ namespace Launcher.Services
     {
         private const string AuthServerUrl = "https://authserver.mojang.com";
         private const string ApiServerUrl = "https://api.mojang.com";
-
-        private readonly IRestClient authClient;
         private readonly IRestClient apiClient;
+        private readonly IRestClient authClient;
         private readonly string clientToken = Settings.Default.ClientToken;
         private string accessToken;
 
@@ -27,32 +25,51 @@ namespace Launcher.Services
             apiClient.BaseUrl = new Uri(ApiServerUrl);
         }
 
-        public bool Authenticate(string email, string password)
+        public async Task<bool> AuthenticateAsync(string email, string password)
         {
-            return Authenticate("/authenticate", new AuthenticationPayload(email, password, clientToken));
+            return await Authenticate("/authenticate", new AuthenticationPayload(email, password, clientToken));
         }
 
-        public bool Authenticate(string accessToken)
+        public async Task<bool> AuthenticateAsync(string accessToken)
         {
-            return Authenticate("/refresh", new TokenPayload(accessToken, clientToken));
+            return await Authenticate("/refresh", new TokenPayload(accessToken, clientToken));
         }
 
-        public UserInfo GetUserInfo()
+        public async Task<bool> LogOff()
+        {
+            try
+            {
+                var request = new RestRequest("/invalidate", Method.POST);
+                request.AddJsonBody(new TokenPayload(accessToken, clientToken));
+                IRestResponse result = await authClient.ExecuteTaskAsync(request);
+
+                Settings.Default.AccessToken = "";
+                Settings.Default.Save();
+
+                return result.StatusCode == HttpStatusCode.NoContent;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<UserInfo> GetUserInfoAsync()
         {
             var request = new RestRequest("/user", Method.GET);
             request.AddHeader("Authorization", "Bearer " + accessToken);
-            var result = apiClient.Execute<UserInfo>(request);
+            var result = await apiClient.ExecuteTaskAsync<UserInfo>(request);
 
             return result.Data;
         }
 
-        private bool Authenticate(string endpoint, IPayload payload)
+        private async Task<bool> Authenticate(string endpoint, IPayload payload)
         {
             try
             {
                 var request = new RestRequest(endpoint, Method.POST);
                 request.AddJsonBody(payload);
-                var result = authClient.Execute<TokenPayload>(request);
+                var result = await authClient.ExecuteTaskAsync<TokenPayload>(request);
 
                 accessToken = result.Data.AccessToken;
                 Settings.Default.AccessToken = accessToken;
