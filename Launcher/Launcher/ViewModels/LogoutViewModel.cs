375f2d1355400aae414f64788ffe8b93fc215388
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-15 00:09:57 +0200

    Added logout

--- Launcher/Launcher/ViewModels/LogoutViewModel.cs
+++ Launcher/Launcher/ViewModels/LogoutViewModel.cs
@@ -1,16 +1,32 @@
-﻿using Launcher.Models;
+﻿using Launcher.Contracts;
+using Launcher.Models;
 using MahApps.Metro.IconPacks;
 
 namespace Launcher.ViewModels
 {
     public sealed class LogoutViewModel : TabBase
     {
-        public LogoutViewModel()
+        private readonly IAccountService accountService;
+        private readonly IMetroWindowManager windowManager;
+
+        public LogoutViewModel(IAccountService accountService, IMetroWindowManager windowManager)
         {
+            this.accountService = accountService;
+            this.windowManager = windowManager;
             DisplayName = "Log Off";
             DisplayIcon = PackIconMaterialKind.LogoutVariant;
             DisplayOrder = 1;
             IsShortTab = true;
+        }
+
+        protected override async void OnViewLoaded(object view)
+        {
+            if (!await accountService.LogOff())
+            {
+                await windowManager.ShowMessageAsync("Error", "Could not log off");
+            }
+
+            windowManager.CloseWindow();
         }
     }
 }
