b45062cd8160f1a063c84409b8e5d1de2f41e981
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-14 20:03:09 +0200

    A new test

---
+++ Launcher/Launcher.Tests/ExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Caliburn.Micro;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Launcher.Extensions;
+using Moq;
+using Ploeh.AutoFixture;
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace Launcher.Tests
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        private readonly IFixture fixture = new Fixture();
+
+        [TestMethod]
+        public void NotifyPropertyChangedEx_ShouldSetAndNotify()
+        {
+            var value = fixture.Create<string>();
+            var value2 = fixture.Create<string>();
+            var propertyName = fixture.Create<string>();
+            var viewModelMock = new Mock<INotifyPropertyChangedEx>();
+            viewModelMock.Setup(x => x.NotifyOfPropertyChange(It.Is<string>(s => s == propertyName)));
+            var viewModel = viewModelMock.Object;
+
+            viewModel.Set(out string field, value, propertyName);
+            viewModel.Set(out field, value2, propertyName);
+
+            Assert.AreEqual(field, value2);
+            viewModelMock.Verify(x => x.NotifyOfPropertyChange(propertyName), Times.Exactly(2));
+        }
+    }
+}
