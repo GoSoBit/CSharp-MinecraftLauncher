d299af2c42a78f35c9ee5b8a80f17aa8f79a8ace
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-17 00:54:35 +0200

    Logout view model tests

---
+++ Launcher/Launcher.Tests/LogoutViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Caliburn.Micro;
+using Launcher.Contracts;
+using Launcher.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Launcher.Tests
+{
+    [TestClass]
+    public class LogoutViewModelTests
+    {
+        [TestMethod]
+        public void ShouldLogOffUsingServiceOnViewLoadedAndClosesWindow()
+        {
+            var accountServiceMock = new Mock<IAccountService>();
+            accountServiceMock.Setup(x => x.LogOffAsync()).ReturnsAsync(true);
+            var windowManagerMock = new Mock<IMetroWindowManager>();
+            var viewModel = new LogoutViewModel(accountServiceMock.Object, windowManagerMock.Object);
+            var fakeView = new UserControl();
+            ViewModelBinder.Bind(viewModel, fakeView, null);
+
+            fakeView.RaiseEvent(new RoutedEventArgs(FrameworkElement.LoadedEvent));
+
+            accountServiceMock.Verify(x => x.LogOffAsync(), Times.Once);
+            windowManagerMock.Verify(x => x.CloseWindow(), Times.Once);
+        }
+    }
+}
