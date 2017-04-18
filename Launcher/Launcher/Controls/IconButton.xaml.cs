de1f36457a9f09548255ad7f3aa922da4cffd42b
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-18 22:54:50 +0200

    Icon Button control

---
+++ Launcher/Launcher/Controls/IconButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using MahApps.Metro.IconPacks;
+
+namespace Launcher.Controls
+{
+    public partial class IconButton
+    {
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.RegisterAttached("Icon", typeof(PackIconModernKind), typeof(IconButton),
+                new FrameworkPropertyMetadata(PackIconModernKind.None, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public IconButton()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public PackIconModernKind Icon
+        {
+            get => (PackIconModernKind)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+    }
+}
