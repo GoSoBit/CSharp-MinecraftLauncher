0b14cf1e789e438c74754b5fee68f5d96db61174
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-19 18:59:22 +0200

    Packs service + model

---
+++ Launcher/Launcher/Contracts/IPacksService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Launcher.Models;
+
+namespace Launcher.Contracts
+{
+    public interface IPacksService
+    {
+        Task<IEnumerable<Pack>> GetAvailablePacksAsync();
+    }
+}
---
+++ Launcher/Launcher/Models/Pack.cs
@@ -0,0 +1,20 @@
+﻿namespace Launcher.Models
+{
+    public class Pack
+    {
+        public Pack()
+        {
+        }
+
+        public Pack(string id, string type, string url)
+        {
+            Id = id;
+            Type = type;
+            Url = url;
+        }
+
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Url { get; set; }
+    }
+}
