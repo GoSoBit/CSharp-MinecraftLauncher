428a97598f06216ce0957857e1e89d31646e9255
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-20 14:50:01 +0200

    New service - serializer

---
+++ Launcher/Launcher/Services/XmlSerializationService.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Launcher.Services
+{
+    public class XmlSerializationService
+    {
+        public string Serialize<T>(T list)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            var writer = new StringWriter();
+            using (writer)
+            {
+                serializer.Serialize(writer, list);
+                return writer.ToString();
+            }
+        }
+
+        public T Deserialize<T>(string xml)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            var reader = new StringReader(xml);
+            using (reader)
+            {
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
