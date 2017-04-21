dc2d13957e573b16c6e891089af8159119b61f6f
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-21 14:43:56 +0200

    Packs service tests

---
+++ Launcher/Launcher.Tests/PacksServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Launcher.Models;
+using Launcher.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RestSharp;
+
+namespace Launcher.Tests
+{
+    [TestClass]
+    public class PacksServiceTests
+    {
+        [TestMethod]
+        public async Task ShouldGetAvailablePacks()
+        {
+            var result = new List<Pack>
+            {
+                new Pack("17w15a", "snapshot", "17w15a.json"),
+                new Pack("17w14a", "snapshot", "17w14a.json"),
+                new Pack("17w13b", "snapshot", "17w13b.json"),
+                new Pack("17w06a", "snapshot", "17w06a.json")
+            };
+            var restClientMock = new Mock<IRestClient>();
+            restClientMock.Setup(x => x.ExecuteTaskAsync<List<Pack>>(It.IsAny<IRestRequest>()))
+                .ReturnsAsync(new RestResponse<List<Pack>> { Data = result });
+            var service = new PacksService(restClientMock.Object);
+
+            var packs = await service.GetAvailablePacksAsync();
+
+            Assert.IsNotNull(result);
+            Assert.AreSame(result, packs);
+            restClientMock.Verify(x => x.ExecuteTaskAsync<List<Pack>>(It.IsAny<IRestRequest>()), Times.Once);
+        }
+    }
+}
