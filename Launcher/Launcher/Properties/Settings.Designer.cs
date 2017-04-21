7e9a248854195e23f5e7b618f6bdd6a493e83acf
Mateusz Łobocki <mlobocki@outlook.com>
2017-04-21 14:41:03 +0200

    User packs can be saved

--- Launcher/Launcher/Properties/Settings.Designer.cs
+++ Launcher/Launcher/Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace Launcher.Properties {
                 this["ClientToken"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string PacksListXml {
+            get {
+                return ((string)(this["PacksListXml"]));
+            }
+            set {
+                this["PacksListXml"] = value;
+            }
+        }
     }
 }
