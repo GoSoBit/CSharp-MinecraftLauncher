2568ee70c3075fe9d2210875ec40983c0c3a9a82
mati9344 <mlobocki@outlook.com>
2017-08-12 14:47:36 +0200

    Better settings handling + 2 new

--- Launcher/Launcher.Desktop/ViewModels/SettingsViewModel.cs
+++ Launcher/Launcher.Desktop/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,4 @@
-﻿using Launcher.Desktop.Extensions;
+﻿using Launcher.Desktop.Contracts;
 using Launcher.Desktop.Models;
 using Launcher.Desktop.Properties;
 using MahApps.Metro.IconPacks;
@@ -7,11 +7,11 @@ namespace Launcher.Desktop.ViewModels
 {
     public sealed class SettingsViewModel : TabBase
     {
-        private bool allowBetaAndAlpha;
-        private bool allowSnapshots;
+        private readonly IMetroWindowManager windowManager;
 
-        public SettingsViewModel()
+        public SettingsViewModel(IMetroWindowManager windowManager)
         {
+            this.windowManager = windowManager;
             DisplayName = "Settings";
             DisplayIcon = PackIconMaterialKind.Settings;
             DisplayOrder = 3;
@@ -19,27 +19,51 @@ namespace Launcher.Desktop.ViewModels
 
         public bool AllowSnapshots
         {
-            get => allowSnapshots;
-            set => this.Set(out allowSnapshots, value);
+            get => LoadSetting<bool>(nameof(AllowSnapshots));
+            set => SaveSetting(nameof(AllowSnapshots), value);
         }
 
         public bool AllowBetaAndAlpha
         {
-            get => allowBetaAndAlpha;
-            set => this.Set(out allowBetaAndAlpha, value);
+            get => LoadSetting<bool>(nameof(AllowBetaAndAlpha));
+            set => SaveSetting(nameof(AllowBetaAndAlpha), value);
         }
 
-        protected override void OnActivate()
+        public string JavaBinDirectory
         {
-            AllowSnapshots = Settings.Default.AllowSnapshots;
-            AllowBetaAndAlpha = Settings.Default.AllowBetaAndAlpha;
+            get => LoadSetting<string>(nameof(JavaBinDirectory));
+            set => SaveSetting(nameof(JavaBinDirectory), value);
+        }
+
+        public short Memory
+        {
+            get => LoadSetting<short>(nameof(Memory));
+            set => SaveSetting(nameof(Memory), value);
+        }
+
+        public void BrowseJavaBinDirectory()
+        {
+            string result = windowManager.ShowDirectoryBrowseDialog(JavaBinDirectory);
+            if (!string.IsNullOrEmpty(result))
+            {
+                JavaBinDirectory = result;
+            }
         }
 
         protected override void OnDeactivate(bool close)
         {
-            Settings.Default.AllowSnapshots = AllowSnapshots;
-            Settings.Default.AllowBetaAndAlpha = AllowBetaAndAlpha;
             Settings.Default.Save();
+        }
+
+        private void SaveSetting(string settingName, object value)
+        {
+            Settings.Default[settingName] = value;
+            NotifyOfPropertyChange(settingName);
+        }
+
+        private T LoadSetting<T>(string settingName)
+        {
+            return (T)Settings.Default[settingName];
         }
     }
 }
